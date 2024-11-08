6bed7b336f2bd58284adb1e88fbe7a16a0e91d36
Namra Vora <voranamra625@gmail.com>
2024-11-08 12:27:50 +0530

    managing routes & providing preiew

--- frontend/src/components/Quote.tsx
+++ frontend/src/components/Quote.tsx
@@ -1,8 +1,8 @@
 export const Quote = () => {
-    return <div className="bg-slate-950 h-screen flex justify-center flex-col">
+    return <div className="bg-gray-950 h-screen flex justify-center flex-col">
     <div className="flex justify-center">
     <div className="max-w-lg">
-    <div className="text-3xl font-bold font-white text-slate-100 text-left">
+    <div className="text-2xl font-bold font-white text-slate-100 text-left">
     "The way to get started is to quit talking and begin doing. Real entrepreneurs build from scratch and challenge the limits, even when the odds are stacked against them."
     </div>
     <div className="max-w-md text-xl font-semibold text-left mt-4 text-slate-300 flex justify-center">
