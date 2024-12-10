e4a3a52c1b5c577281264e6fe3834231a689268c
Namra Vora <voranamra625@gmail.com>
2024-12-10 18:18:18 +0530

    solve error

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -19,7 +19,7 @@ blogRouter.use("/*", async (c, next) => {
     try {
         const user = await verify(authHeader, c.env.JWT_SECRET);
         if (user) {
-            c.set("userId", user.id);
+            c.set("userId", user.id as string);
             await next();
         } else {
             c.status(403);
